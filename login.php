ec6a3b5405bd891ced3ba940ebde6bbc6a297728
Muaadh <105169441+muaadh99@users.noreply.github.com>
2023-10-19 22:08:23 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 $google_client_id = "132686350965-o3qg2f3oedc2v0ajhf1oq0p8dnh4b0t2.apps.googleusercontent.com";
-$redirect_url = "http://muaadh99.github.io/redirect.php";
+$redirect_url = "https://muaadh99.github.io/CS3400-CyberSecurity_Project/redirrect.php";
 $google_auth_url = "https://accounts.google.com/o/oauth2/auth?client_id={$google_client_id}&redirect_uri={$redirect_url}&scope=email&response_type=code";
 ?>
 
